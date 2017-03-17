19f2f5a8c5d5740c8046bd896cedf5448dd9211e
AaronLai <aaronlaiya@icloud.com>
2017-03-17 16:57:56 +0800

    add GAN

---
+++ GAN/GAN.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# @Author: aaronlai
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(3 * 32 * 32, 256)
+        self.drop1 = nn.Dropout(p=0.2)
+        self.fc2 = nn.Linear(256, 128)
+        self.drop2 = nn.Dropout(p=0.2)
+        self.fc3 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = self.drop1(F.leaky_relu(self.fc1(x)))
+        x = self.drop2(F.leaky_relu(self.fc2(x)))
+        x = F.sigmoid(self.fc3(x))
+        return x
+
+
+class Generator(nn.Module):
+
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.fc1 = nn.Linear(2, 256)
+        self.fc2 = nn.Linear(256, 512)
+        self.fc3 = nn.Linear(512, 1024)
+        self.fc4 = nn.Linear(1024, 32 * 32 * 3)
+
+    def forward(self, x):
+        x = F.elu(self.fc1(x))
+        x = F.elu(self.fc2(x))
+        x = F.elu(self.fc3(x))
+        x = F.tanh(self.fc4(x))
+        return x
---
+++ GAN/run_GAN.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+# @Author: aaronlai
+
+import torch
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+from torch.autograd import Variable
+from GAN import Discriminator, Generator
+
+
+def load_CIFAR10(batch_size=10, download=True):
+    """
+    The output of torchvision datasets are PILImage images of range [0, 1].
+    Transform them to Tensors of normalized range [-1, 1]
+    """
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5),
+                                                         (0.5, 0.5, 0.5))])
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=download,
+                                            transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=download,
+                                           transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return trainloader, testloader, classes
+
+
+def gen_noise(n_instance):
+    """generate 2-dim uniform random noise"""
+    return torch.Tensor(np.random.uniform(low=-1.0, high=1.0,
+                                          size=(n_instance, 2)))
+
+
+def train_GAN(Dis_model, Gen_model, D_criterion, G_criterion, D_optimizer,
+              G_optimizer, trainloader, n_epoch, batch_size,
+              n_update_dis=1, n_update_gen=1, use_gpu=False, print_every=100):
+    """train GAN and print out the losses for D and G"""
+    for epoch in range(n_epoch):
+
+        D_running_loss = 0.0
+        G_running_loss = 0.0
+
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs from true distribution
+            true_inputs, _ = data
+            true_inputs = true_inputs.view(-1, 3 * 32 * 32)
+            if use_gpu:
+                true_inputs = true_inputs.cuda()
+            true_inputs = Variable(true_inputs)
+
+            # get the inputs from the generator
+            noises = gen_noise(batch_size)
+            if use_gpu:
+                noises = noises.cuda()
+            fake_inputs = Gen_model(Variable(noises))
+            inputs = torch.cat([true_inputs, fake_inputs])
+
+            # get the labels
+            labels = np.zeros(2 * batch_size)
+            labels[:batch_size] = 1
+            labels = torch.from_numpy(labels.astype(np.float32))
+            if use_gpu:
+                labels = labels.cuda()
+            labels = Variable(labels)
+
+            # Discriminator
+            D_optimizer.zero_grad()
+            outputs = Dis_model(inputs)
+            D_loss = D_criterion(outputs[:, 0], labels)
+            if i % n_update_dis == 0:
+                D_loss.backward(retain_variables=True)
+                D_optimizer.step()
+
+            # Generator
+            if i % n_update_gen == 0:
+                G_optimizer.zero_grad()
+                G_loss = G_criterion(outputs[batch_size:, 0],
+                                     labels[:batch_size])
+                G_loss.backward()
+                G_optimizer.step()
+
+            # print statistics
+            D_running_loss += D_loss.data[0]
+            G_running_loss += G_loss.data[0]
+            if i % print_every == (print_every - 1):
+                print('[%d, %5d] D loss: %.3f ; G loss: %.3f' %
+                      (epoch+1, i+1, D_running_loss / print_every,
+                       G_running_loss / print_every))
+                D_running_loss = 0.0
+                G_running_loss = 0.0
+
+    print('Finished Training')
+
+
+def main(n_epoch=2, batch_size=10, use_gpu=False, dis_lr=0.001, gen_lr=0.01,
+         n_update_dis=1, n_update_gen=1):
+    # loading data
+    trainloader, testloader, classes = load_CIFAR10(batch_size=batch_size)
+
+    # initialize models
+    Dis_model = Discriminator()
+    Gen_model = Generator()
+
+    if use_gpu:
+        Dis_model = Dis_model.cuda()
+        Gen_model = Gen_model.cuda()
+
+    # assign loss function and optimizer to D and G
+    D_criterion = torch.nn.BCELoss()
+    D_optimizer = optim.SGD(Dis_model.parameters(), lr=dis_lr, momentum=0.9)
+
+    G_criterion = torch.nn.BCELoss()
+    G_optimizer = optim.SGD(Gen_model.parameters(), lr=gen_lr, momentum=0.9)
+
+    train_GAN(Dis_model, Gen_model, D_criterion, G_criterion, D_optimizer,
+              G_optimizer, trainloader, n_epoch, batch_size, n_update_dis,
+              n_update_gen)
+
+
+if __name__ == '__main__':
+    main()
