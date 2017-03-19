43fd9c30d6a8213e2808745ebf97172f48ed1bcf
AaronLai <aaronlaiya@icloud.com>
2017-03-20 00:40:23 +0800

    add G and D

--- DCGAN/DCGAN.py
+++ DCGAN/DCGAN.py
@@ -1,3 +1,23 @@
 # -*- coding: utf-8 -*-
 # @Author: aaronlai
 
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DCGAN_Discriminator(nn.Module):
+
+    def __init__(self):
+        super(DCGAN_Discriminator, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
+class DCGAN_Generator(nn.Module):
+
+    def __init__(self):
+        super(DCGAN_Generator, self).__init__()
+
+    def forward(self, x):
+        return x
