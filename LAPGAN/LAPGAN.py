aba64d41d5c0fbe8e9af0247311f56828c7f5a6a
AaronLai <aaronlaiya@icloud.com>
2017-03-23 21:23:49 +0800

    init LAPGAN

---
+++ LAPGAN/LAPGAN.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: aaronlai
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LAPGAN_Discriminator(nn.Module):
+
+    def __init__(self):
+
+    def forward(self, x):
+
+
+class LAPGAN_Generator(nn.Module):
+
+    def __init__(self):
+
+    def forward(self, x):
